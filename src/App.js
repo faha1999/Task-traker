671c7bd5b484455aa36a3dbc352e737145fafbdc
faha1999 <kafaha1999@gmail.com>
2022-04-12 02:02:59 +0600

    Build for production & added json server

--- src/App.js
+++ src/App.js
@@ -7,35 +7,7 @@ import AddTask from './components/AddTask';
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
 
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'doctor appointment',
-      day: 'reb 5th at 2.30 pm',
-      reminder: true,
-    },
-
-    {
-      id: 2,
-      text: 'doctor appointment 2',
-      day: 'reb 5th at 2.30 pm',
-      reminder: true,
-    },
-
-    {
-      id: 3,
-      text: 'doctor appointment 3',
-      day: 'reb 5th at 2.30 pm',
-      reminder: false,
-    },
-
-    {
-      id: 4,
-      text: 'doctor appointment 4',
-      day: 'reb 5th at 2.30 pm',
-      reminder: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   // Add task
   const addTask = (task) => {
