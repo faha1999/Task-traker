2730d70911c96a0f5348afa2c483f77b944ba22c
faha1999 <kafaha1999@gmail.com>
2022-04-12 00:27:28 +0600

    applied logic on Button toggle

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -58,8 +60,11 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,25 @@
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = (props) => {
-  const onClick = () => {
-    console.log('click');
-  };
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
-      <h1>{props.title}</h1>
-      <Button color="green" text="Add" onClick={onClick} />
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? 'red' : 'green'}
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onAdd}
+      />
     </header>
   );
 };
 
 Header.defaultProps = {
-  title: 'task tracker',
+  title: 'Task Tracker',
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default Header;
