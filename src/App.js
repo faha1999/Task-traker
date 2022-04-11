19da2645e05b54abc412a23671da3200d0eb9829
faha1999 <kafaha1999@gmail.com>
2022-04-11 23:51:29 +0600

    showAddTask state

--- src/App.js
+++ src/App.js
@@ -35,6 +35,13 @@ function App() {
     },
   ]);
 
+  // Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   // Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -52,7 +59,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
