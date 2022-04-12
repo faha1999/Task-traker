55e96a852c63808c2f4b8741d3f04c2a521c3cec
faha1999 <kafaha1999@gmail.com>
2022-04-13 02:18:28 +0600

    Toggle reminder on server

--- src/App.js
+++ src/App.js
@@ -17,9 +17,16 @@ function App() {
     getTasks();
   }, []);
 
-  // Fetch task
+  // Fetch tasks
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    return data;
+  };
+
+  // Fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
     const data = await res.json();
     return data;
   };
@@ -55,10 +62,23 @@ function App() {
   };
 
   // Toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const upTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(upTask),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task,
+        task.id === id ? { ...task, reminder: !data.reminder } : task,
       ),
     );
   };
